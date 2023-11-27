8e8a56f10cb32f514445bb28511648a48ab02027
nuhptr <nugrahaadi733@gmail.com>
2023-11-27 23:30:44 +0700

    components/update code

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import Image from "next/image"
+import { MotionDiv } from "./MotionDiv"
+import { Variants } from "framer-motion"
 
 export interface AnimeProps {
    id: string
@@ -17,13 +21,23 @@ interface Props {
    index: number
 }
 
-export default function AnimeCard({ anime }: Props) {
+const variants: Variants = {
+   hidden: { opacity: 0 },
+   visible: { opacity: 1 },
+}
+
+export default function AnimeCard({ anime, index }: Props) {
    return (
-      <div className="relative w-full max-w-sm rounded">
+      <MotionDiv
+         variants={variants}
+         initial="hidden"
+         animate="visible"
+         transition={{ delay: index * 0.25, ease: "easeInOut", duration: 0.5 }}
+         className="relative w-full max-w-sm rounded">
          <div className="relative w-full h-[37vh]">
-            <Image src={anime.image.original} alt={anime.name} fill className="rounded-xl" />
+            <Image src={`https://shikimori.one${anime.image.original}`} alt={anime.name} fill className="rounded-xl" />
          </div>
-         <div className="flex flex-col gap-3 py-4">
+         <div className="flex flex-col py-4 gap-3">
             <div className="flex items-center justify-between gap-1">
                <h2 className="w-full text-xl font-bold text-white line-clamp-1">{anime.name}</h2>
                <div className="py-1 px-2 bg-[#161921] rounded-sm">
@@ -32,15 +46,15 @@ export default function AnimeCard({ anime }: Props) {
             </div>
             <div className="flex items-center gap-4">
                <div className="flex flex-row items-center gap-2">
-                  <Image src="./episodes.svg" alt="episodes" width={20} height={20} className="object-contain" />
+                  <Image src="/ic_episodes.svg" alt="episodes" width={20} height={20} className="object-contain" />
                   <p className="text-base font-bold text-white">{anime.episodes || anime.episodes_aired}</p>
                </div>
                <div className="flex flex-row items-center gap-2">
-                  <Image src="./star.svg" alt="star" width={18} height={18} className="object-contain" />
+                  <Image src="/ic_star.svg" alt="star" width={18} height={18} className="object-contain" />
                   <p className="text-base font-bold text-[#FFAD49]">{anime.score}</p>
                </div>
             </div>
          </div>
-      </div>
+      </MotionDiv>
    )
 }
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,11 +1,39 @@
+"use client"
+
 import Image from "next/image"
+import { useEffect, useState } from "react"
+import { useInView } from "react-intersection-observer"
+
+import { fetchAnime } from "@/app/action"
+import AnimeCard from "./AnimeCard"
+
+let page = 2
+
+export type AnimeCard = JSX.Element
 
 export default function LoadMore() {
+   const { ref, inView } = useInView()
+   const [data, setData] = useState<AnimeCard[]>([])
+
+   useEffect(() => {
+      if (inView) {
+         console.log("Load more")
+         //? Infinite load
+         fetchAnime(page).then((response) => {
+            setData([...data, ...response])
+            page++
+         })
+      }
+   }, [data, inView])
+
    return (
-      <section className="flex items-center justify-center w-full">
-         <div>
-            <Image src="./spinner.svg" alt="spinner" width={56} height={56} className="object-contain" />
-         </div>
-      </section>
+      <>
+         <section className="grid grid-cols-1 gap-10 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">{data}</section>
+         <section className="flex items-center justify-center w-full">
+            <div ref={ref}>
+               <Image src="/ic_spinner.svg" alt="spinner" width={56} height={56} className="object-contain" />
+            </div>
+         </section>
+      </>
    )
 }
